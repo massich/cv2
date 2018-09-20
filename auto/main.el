39c18828a5204c6524b010321a1c7b1e975e9c32
Joan Massich <sik@visor.udg.edu>
2018-09-20 15:51:26 +0200

    Initial commit

---
+++ auto/main.el
@@ -0,0 +1,48 @@
+(TeX-add-style-hook
+ "main"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("moderncv" "12pt" "a4paper" "sans")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("geometry" "scale=0.88") ("biblatex" "backend=bibtex8" "url=false" "style=authoryear" "doi=false" "isbn=false" "maxnames=99" "dashed=false" "sorting=ydnt")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "./content/contactInfo"
+    "./content/experience"
+    "./content/educationLong"
+    "./content/thesis"
+    "moderncv"
+    "moderncv12"
+    "inputenc"
+    "geometry"
+    "pdfpages/pdfpages"
+    "moderntimeline/moderntimeline"
+    "xpatch/xpatch"
+    "color"
+    "graphicx"
+    "biblatex")
+   (TeX-add-symbols
+    '("cvreference" 8)
+    '("cvreferencecolumn" 2)
+    "aj"
+    "araa"
+    "apj"
+    "apjl"
+    "apjs"
+    "apss"
+    "aap"
+    "aapr"
+    "aaps"
+    "mnras"
+    "pasp"
+    "pasj"
+    "qjras"
+    "nat"
+    "aplett"
+    "aas"
+    "solphys"
+    "memsai")
+   (LaTeX-add-bibliographies
+    "publications"))
+ :latex)
+
