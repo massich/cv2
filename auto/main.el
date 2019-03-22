03f8327cedaa96b8efc9f9e9c8c34d801f9c2142
Joan Massich <sik@visor.udg.edu>
2019-03-22 18:41:50 +0100

    add R2b2

--- auto/main.el
+++ auto/main.el
@@ -5,6 +5,10 @@
                      '(("moderncv" "12pt" "a4paper" "sans")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("geometry" "scale=0.88") ("biblatex" "backend=bibtex8" "url=false" "style=authoryear" "doi=false" "isbn=false" "maxnames=99" "dashed=false" "sorting=ydnt")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "./content/contactInfo"
