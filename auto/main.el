f9d72fad9aae7d7e1b3d557909fe49e581c86519
Joan Massich <sik@visor.udg.edu>
2019-03-28 09:17:49 +0100

    better

--- auto/main.el
+++ auto/main.el
@@ -4,17 +4,18 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("moderncv" "12pt" "a4paper" "sans")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("geometry" "scale=0.88") ("biblatex" "backend=bibtex8" "url=false" "style=authoryear" "doi=false" "isbn=false" "maxnames=99" "dashed=false" "sorting=ydnt")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+                     '(("inputenc" "utf8") ("geometry" "scale=0.88") ("biblatex" "backend=bibtex8" "url=false" "style=authoryear" "doi=false" "isbn=false" "maxnames=99" "maxcitenames=1" "dashed=false" "sorting=ydnt")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "./content/contactInfo"
     "./content/experience"
     "./content/educationLong"
     "./content/thesis"
+    "./content/publicationsLong"
     "moderncv"
     "moderncv12"
     "fontawesome"
@@ -23,6 +24,7 @@
     "pdfpages/pdfpages"
     "moderntimeline/moderntimeline"
     "xpatch/xpatch"
+    "amsmath"
     "color"
     "graphicx"
     "biblatex")
